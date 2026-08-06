4db83949b9367e29fd1127b9e0628c9c586e7563
Mani Sasank Reddy <ymani99123@gmail.com>
2026-08-06 22:33:52 +0530

    Revert "python daily learning"
    This reverts commit a0f89bf88c32d339b794629bf10e9857458fd4e4.

--- text_to_voice.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-
-Text = "Hello, this is a text to speech conversion using gTTS library."
-
-tts = gTTS(text=Text, lang='en')
-tts.save("voice.mp3")
-print("audio saved successfully")
